3f5ded68449fbdda42394055cb4389884db7481e
Gustavo Machado <machadogj@gmail.com>
2015-12-22 15:54:02 -0300

    [minor] re-use nextFrame, remove try/catch

--- index.js
+++ index.js
@@ -2,33 +2,23 @@
 /* Returns a promise that will resolve on the next AnimationFrame */
 function nextFrame() {
   return new Promise(function(resolve, reject) {
-    requestAnimationFrame(function() { resolve(); });
+    requestAnimationFrame(resolve);
   });
 }
 
 /* Applies `fn` to each element of `collection`, iterating once per frame */
 nextFrame.mapInFrames = function(collection, fn) {
   var results = [];
-
-  return new Promise(function(resolve, reject) {
-    var processEntry = function(index) {
-      if (index < collection.length) {
-        requestAnimationFrame(function() {
-          try {
-            results.push(fn(collection[index]));
-            processEntry(index+1);
-          } catch(e) {
-            reject(e);
-          }
-        })
-      }
-      else {
-        resolve(results);
-      }
-    };
-
-    processEntry(0);
-  });
+  function processEntry () {
+    if (results.length === collection.length) {
+      return new Promise(function (resolve) { resolve(results); });
+    }
+    return nextFrame().then(function () {
+      results.push(fn(collection[results.length]));
+      return processEntry();
+    });
+  }
+  return processEntry();
 }
 
 module.exports = nextFrame;
